374560e886164650bf358bcff7be3369f69f5ed6
Bohdan Yarinko <yarinko.b@gmail.com>
2024-07-24 18:00:19 +0300

    added .env

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from aiogram import Bot, Dispatcher
 from app.handlers import router
 from app.database.models import async_main
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 async def main():
     await async_main()
-    bot = Bot(token='7137165503:AAHvlIR6-VI8myJKythkbJg9fndpU-LNf2M')
+    bot = Bot(token=os.getenv('BOT_TOKEN'))
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
