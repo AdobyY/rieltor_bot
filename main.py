c824210c5baf90d75f4f8ca7d4135a04fa2c1c70
AdobyY <81767251+AdobyY@users.noreply.github.com>
2024-08-27 08:09:24 +0000

    added logging

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import asyncio
+import logging
 from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv
+import os
 
 from app.handlers import router
 from app.database.models import async_main
 
-from dotenv import load_dotenv
-import os
 
 load_dotenv()
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("bot.log"),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger(__name__)
 
 async def main():
     await async_main()
     bot = Bot(token=os.getenv('BOT_TOKEN'))
     dp = Dispatcher()
     dp.include_router(router)
+    logger.info("Бот запущено і готовий до роботи")
+
     await dp.start_polling(bot)
 
 if __name__=='__main__':
